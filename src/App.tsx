83b50803d9ad51a3870ce2dddaef1a1b45204896
kongpeng <kongpeng@daixiaomi.com>
2019-06-06 14:52:10 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Button } from 'antd';
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <h1>
+          <Button type="primary">button</Button>
+        </h1>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
